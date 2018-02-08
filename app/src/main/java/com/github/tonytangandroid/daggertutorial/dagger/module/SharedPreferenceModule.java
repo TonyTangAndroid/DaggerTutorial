6a0f29654d2ee0baaa1ad2f5d5f138cf26f2fc37
ztang <ztang@microstrategy.com>
2018-02-08 16:38:45 -0500

    1, Add a shared preference module.  2, Include the shared preference module in application component.  3, Expose the shared preference interface.  4, Create a Activity component which depends on the application component.  5, Inject the activity to initial the shared preference and context in the main activity.

---
+++ app/src/main/java/com/github/tonytangandroid/daggertutorial/dagger/module/SharedPreferenceModule.java
@@ -0,0 +1,18 @@
+package com.github.tonytangandroid.daggertutorial.dagger.module;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class SharedPreferenceModule {
+
+    @Provides
+    SharedPreferences provideSharedPreferences(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+}
