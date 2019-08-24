f7d80a856fc8f9d9c15d104e9c9e3fbac12f864d
Tony Tang <tonytang@uber.com>
2019-08-23 19:03:33 -0700

    Fix android test.

--- app/src/androidTest/java/com/github/tonytangandroid/daggertutorial/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/github/tonytangandroid/daggertutorial/ExampleInstrumentedTest.java
@@ -19,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.github.tonytangandroid.daggertutorial", appContext.getPackageName());
     }
