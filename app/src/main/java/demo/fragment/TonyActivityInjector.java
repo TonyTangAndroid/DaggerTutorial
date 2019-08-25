85266cd64cd88a8558bc7f8fdc5d96702fdf1d46
TonyTandAndroid <tonytangandroid@gmail.com>
2019-08-24 23:56:02 -0700

    Add example of injecting activity without using @ContributeAndInjector

--- app/src/main/java/demo/fragment/TonyActivityInjector.java
+++ app/src/main/java/demo/fragment/TonyActivityInjector.java
@@ -1,14 +1,29 @@
 package demo.fragment;
 
+import dagger.Binds;
 import dagger.Module;
-import dagger.android.ContributesAndroidInjector;
+import dagger.Subcomponent;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
+import demo.fragment.TonyActivityInjector.YourActivitySubcomponent.Factory;
 
-@Module
-public abstract class TonyActivityInjector {
+@Module(subcomponents = TonyActivityInjector.YourActivitySubcomponent.class)
+abstract class TonyActivityInjector {
+    @Binds
+    @IntoMap
+    @ClassKey(TonyActivity.class)
+    abstract AndroidInjector.Factory<?> bindYourAndroidInjectorFactory(Factory factory);
 
-    @ContributesAndroidInjector(modules = {TonyActivityModule.class,
-            FragmentFactoryModule.class})
-    abstract TonyActivity bindTonyActivity();
+    @Subcomponent(modules = {
+            TonyActivityModule.class,
+            FragmentFactoryModule.class
+    })
+    public interface YourActivitySubcomponent extends AndroidInjector<TonyActivity> {
+        @Subcomponent.Factory
+        interface Factory extends AndroidInjector.Factory<TonyActivity> {
 
 
+        }
+    }
 }
