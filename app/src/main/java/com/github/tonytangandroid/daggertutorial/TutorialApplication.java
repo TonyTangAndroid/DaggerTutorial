3ac3d7b385a7d1a04bcd88336ae8eb669b2949dd
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-08-06 21:44:01 -0700

    Revert "Migrate DI to Dagger Hilt"
    This reverts commit fea2d076

--- app/src/main/java/com/github/tonytangandroid/daggertutorial/TutorialApplication.java
+++ app/src/main/java/com/github/tonytangandroid/daggertutorial/TutorialApplication.java
@@ -1,15 +1,30 @@
 package com.github.tonytangandroid.daggertutorial;
 
 import android.app.Application;
-import dagger.hilt.android.HiltAndroidApp;
+import com.github.tonytangandroid.daggertutorial.dagger.DaggerAppComponent;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasAndroidInjector;
+import javax.inject.Inject;
 import timber.log.Timber;
 
-@HiltAndroidApp
-public class TutorialApplication extends Application {
+public class TutorialApplication extends Application implements HasAndroidInjector {
+
+  @Inject DispatchingAndroidInjector<Object> dispatchingAndroidInjector;
 
   @Override
   public void onCreate() {
+    initDagger();
     super.onCreate();
     Timber.plant(new Timber.DebugTree());
+  }
+
+  @Override
+  public AndroidInjector<Object> androidInjector() {
+    return dispatchingAndroidInjector;
+  }
+
+  private void initDagger() {
+    DaggerAppComponent.builder().application(this).build().inject(this);
   }
 }
--- app/src/main/java/com/github/tonytangandroid/daggertutorial/dagger/module/ApplicationModule.java
+++ app/src/main/java/com/github/tonytangandroid/daggertutorial/dagger/module/ApplicationModule.java
@@ -4,11 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import dagger.Module;
 import dagger.Provides;
-import dagger.hilt.InstallIn;
-import dagger.hilt.android.components.ApplicationComponent;
 
 @Module
-@InstallIn(ApplicationComponent.class)
 public class ApplicationModule {
 
   @Provides
